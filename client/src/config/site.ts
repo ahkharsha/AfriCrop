40bf88b009ee00cbc94213e7d5b7b041834793e0
ahkharsha <ahkharsha@gmail.com>
2025-06-21 13:28:08 +0530

    Update frontend

--- client/src/config/site.ts
+++
@@ -1,9 +0,0 @@
-export const siteConfig = {
-  name: "AfriCrop DAO",
-  description: "Decentralized agricultural governance for African farmers",
-  links: {
-    github: "https://github.com/africrop-dao",
-    twitter: "https://twitter.com/africropdao",
-    discord: "https://discord.gg/africrop",
-  },
-}
